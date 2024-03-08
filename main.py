3ee022bd7a67d20b768104ed6940152f3ae7d3ec
Vitrinkutusu <162738713+Vitrinkutusu@users.noreply.github.com>
2024-03-08 21:26:03 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "ROFL" : "bir şakaya karşılık cevap"
+            }
+            
+while True:            
+    word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    # Kelime eşleşiyorsa ne yapmalıyız?
+    else:
+        print("Lütfen doğru bir kelime yazın:")
+    # Kelime eşleşmiyorsa ne yapmalıyız?
